8c7e64ab36d501ba2566ab68fb306c2bae077ed4
Sravan Vankina <spvankina@gmail.com>
2023-08-31 17:03:21 +0530

    fixed wrong function name

--- custom_components/control4/cover.py
+++ custom_components/control4/cover.py
@@ -96,7 +96,7 @@ class Control4Cover(Control4Entity, CoverEntity):
         await c4_cover.open()
         await self.async_update_ha_state(force_refresh=True)
 
-    async def async_close(self, **kwargs) -> None:
+    async def async_close_cover(self, **kwargs) -> None:
         """Close the cover"""
         c4_cover = self.create_api_object()
         await c4_cover.close()
